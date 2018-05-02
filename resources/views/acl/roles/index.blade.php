b60616ec4a40c2818d23936f58143d6a4779c516
tharidnarodrigo <tharindarodrigo@gmail.com>
2018-05-02 17:17:47 +0530

    Roles CRUD module for Admins added

--- resources/views/acl/roles/index.blade.php
+++ resources/views/acl/roles/index.blade.php
@@ -59,8 +59,6 @@
 @endpush
 
 @push('js')
-    {{--<script src="{!! asset('js/app.js') !!}"></script>--}}
-    {{--<script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.js"></script>--}}
     <script>
         $(document).ready(function () {
             $('#buttons').DataTable();
