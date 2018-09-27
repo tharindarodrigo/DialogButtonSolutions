a82771ecdf7669fd81a8eb6ad9ee0e7ed28a6946
Tharinda Rodrigo <tharindarodrigo@gmail.com>
2018-09-27 12:28:10 +0530

    excel export for buttons

--- app/Exports/ButtonClicksExport.php
+++ app/Exports/ButtonClicksExport.php
@@ -4,14 +4,21 @@ namespace App\Exports;
 
 use App\ButtonClick;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithMapping;
 
 class ButtonClicksExport implements FromCollection
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
-        return ButtonClick::all();
+        return ButtonClick::whereHas('buttonType', function ($q1) {
+            $q1->select('button_type');
+        })->whereHas('company', function ($q2) {
+            $q2->select('name');
+        })->whereHas('branch', function ($q3) {
+            $q3->select('branch');
+        })->get();
     }
 }
