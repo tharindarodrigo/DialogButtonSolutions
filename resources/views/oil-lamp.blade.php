efb880bf907109fbd5bbb7c92ab75d7cd4c5cfe0
Tharinda Rodrigo <tharindarodrigo@gmail.com>
2018-12-12 07:36:30 +0530

    Oil Lamp

--- resources/views/oil-lamp.blade.php
+++ resources/views/oil-lamp.blade.php
@@ -75,7 +75,7 @@
 <script>
     $(document).ready(function () {
 
-        var lampElement = '<div class="col-md-4 img-el">' +
+        var lampElement = '<div class="col-md-4 img-el" style="display:none;">' +
             '<img src="{{ asset("img/on.gif") }}" alt="" class="image center">' +
             '</div>';
 
